34ac92bc801c52c0d19b40764ed547645a87949c
oumbarae hachmahocine <hachmaoui@hotmail.fr>
2023-04-11 04:45:17 +0200

    Ajoutez des fichiers projet.

---
+++ MediaTekDocuments/model/CmdLivre.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaTekDocuments.model
+{
+    public class CmdLivre: Commande
+    {
+        public string Etape { get; set; }
+        public int Quantité { get; set; }
+        public string idLivreDvd { get; set; }
+
+        public CmdLivre(string etape, int nbExemplaire, string livre, string id, DateTime dateCommande, int montant) : base(id, dateCommande, montant)
+        //base(id,dateCommande,montant)
+        {
+            Etape = etape;
+            Quantité = nbExemplaire;
+            idLivreDvd = livre;
+        }
+    }
+}
